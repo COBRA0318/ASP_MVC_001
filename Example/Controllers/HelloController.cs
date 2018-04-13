e9161886aca4631d5384c878b58671923d38c6ae
COBRA0318 <ryo_chan@d5.dion.ne.jp>
2018-04-13 11:45:59 +0900

    modelとビューを追加しました

---
+++ Example/Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿using Example.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Example.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult HelloView()
+		{
+			Hello hl = new Hello();
+			hl.Str1 = this.Request.QueryString["prm1"];
+			hl.Str2 = this.Request.QueryString["prm2"];
+			return View(hl);
+		}
+
+    }
+}
---
+++ Example/Models/Hello.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Example.Models
+{
+	public class Hello
+	{
+		public string Str1 { get; set; }
+		public string Str2 { get; set; }
+	}
+}
